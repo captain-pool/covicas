1bbd936daafa609c3c1fdc577118aea95d6ecc90
captain-pool <rickdey1998@gmail.com>
2019-02-19 18:05:40 +0530

    Added some important lines

---
+++ __set_global_host.py
@@ -0,0 +1,3 @@
+from covicas.settings import settings
+s = settings("abcd.json")
+s.set("host","0.0.0.0")
--- __tester.py
+++ __tester.py
@@ -2,15 +2,33 @@ from covicas.model import LBPHClassifier as lc
 from covicas.face_extract import Extract
 from covicas.api import Response
 import cv2
+from absl import app,flags
 from covicas.settings import settings
-print("Booting Up ...")
-s = settings("abcd.json")
-#s.set("debug",True)
-e = Extract(source = 1,channel = "GRAY")
-m = lc(type = lc.EXEC,frame_gen = e,trained_model = "saved_model.yml")
-gen = m.read()
-print("[READY]")
-r = Response(generator = gen,settings = s)
-r.run()
-if s.get("debug",False):
-    cv2.destroyAllWindows()
+FLAGS = flags.FLAGS
+flags.DEFINE_string("cam","0","<WebCamera Number>/ raspi")
+flags.DEFINE_string("settings","abcd.json","Settings File to use")
+def main(argv):
+    del argv
+    raspi = False
+    cam_num = 0
+    if FLAGS.cam == "raspi":
+        raspi = True
+    else:
+        try:
+            cam_num = int(FLAGS.cam)
+        except:
+            raise ValueError    
+        print("Booting Up ...")
+        s = settings(FLAGS.settings)
+        #s.set("debug",True)
+        print(raspi,cam_num)
+        e = Extract(source = cam_num,channel = "GRAY",raspi = raspi)
+        m = lc(type = lc.EXEC,frame_gen = e,trained_model = "saved_model.yml")
+        gen = m.read()
+        print("[READY]")
+        r = Response(generator = gen,settings = s)
+        r.run()
+        if s.get("debug",False):
+            cv2.destroyAllWindows()
+if __name__ == "__main__":
+    app.run(main)
