18d982a2efa76e816a2d4733db8d5d9a216cd1c4
captain-pool <rickdey1998@gmail.com>
2019-02-20 18:06:10 +0530

    Updated install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,4 @@ sudo apt-get install -y libgtk-3-dev
 sudo apt-get install -y libboost-all-dev
 wget -c http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
 bzip2 -d shape_predictor_68_face_landmarks.dat.bz2
-pip3 install scipy
-pip3 install scikit-image
-pip3 install dlib
+sudo pip3 install -r requirements.txt
